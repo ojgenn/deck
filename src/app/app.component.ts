da4719bde0294b10bd9a536bbce2b23402c15f1f
ojgenn <ojgenn@gmail.comgit>
2019-07-11 23:28:42 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import * as deckConstants from './commons/constants/suits-and-faces';
+
+interface DeckInterface {
+  suit: string;
+  face: string;
+  color: string;
+}
+
+const CARDS_TO_SHOW = 5;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'deck';
+  deck: Deck = new Deck();
+  deckToShow: Array<DeckInterface> = [];
+  constructor() {
+  }
+
+  deal(): void {
+    this.deck.deal(CARDS_TO_SHOW);
+    this.deckToShow = this.deck.getDeckToShow();
+  }
+
+  clear(): void {
+    this.deck.clear();
+    this.deckToShow = this.deck.getDeckToShow();
+  }
+}
+
+class Deck {
+  private _deck = this._initDeck();
+  private _deckToShow: Array<DeckInterface> = [];
+
+  deal(num): void {
+    const deckToShow: Array<DeckInterface> = [];
+    for (let i = 0; i < num; i++) {
+      const randomIndex = getRandomInt(0, this._deck.length);
+      if (!this._deck[randomIndex]) {
+        this._deck = this._initDeck();
+      }
+      deckToShow.push(this._deck[randomIndex]);
+      this._deck.splice(randomIndex, 1);
+    }
+
+    this._deckToShow = [
+      ...this._deckToShow,
+      ...deckToShow,
+    ];
+  }
+
+  clear(): void {
+    this._deckToShow = [];
+  }
+
+  getDeckToShow(): Array<DeckInterface> {
+    return this._deckToShow;
+  }
+
+  private _initDeck(): Array<DeckInterface> {
+    const deck = [];
+    deckConstants.suits.forEach(suit => {
+      deckConstants.faces.forEach(face => {
+        deck.push({suit, face, color: deckConstants.suitsColors[suit]});
+      });
+    });
+    return deck;
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
---
+++ src/app/commons/constants/suits-and-faces.ts
@@ -0,0 +1,5 @@
+export const suits = ['♠', '♣', '♥', '♦'];
+export const faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+export const suitsColors = {
+  '♠': 'black', '♣': 'black', '♥': 'red', '♦': 'red',
+};
